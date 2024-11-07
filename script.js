a31515742a5a9adb9b8dad6e5ac50333c4e931f3
shanka-r <103333587+shanka-r@users.noreply.github.com>
2024-11-07 07:40:56 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function off(){
-   document.getElementById("off").src="https://media.istockphoto.com/photos/isolated-shot-of-illuminated-light-bulb-on-white-background-picture-id480003160?k=6&m=480003160&s=170667a&w=0&h=xQQvhPkNOjUyMOHmM2_eDfW6WIFiCfhEb1wH68m4oEA="
-}
-function on(){
-   document.getElementById("off").src="https://st.depositphotos.com/1942805/2400/i/600/depositphotos_24001349-stock-photo-light-bulb.jpg"}
